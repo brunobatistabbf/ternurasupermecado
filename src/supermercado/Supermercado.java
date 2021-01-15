64a05278a40e331cb91c0a14522966ecff4ce856
Bruno Batista <43976638+brunobatistabbf@users.noreply.github.com>
2021-01-15 06:07:01 -0200

    commit menu

--- src/supermercado/Supermercado.java
+++ src/supermercado/Supermercado.java
@@ -9,7 +9,15 @@ package supermercado;
 public class Supermercado {
     
     public static void main(String[] args) {
-        
+        System.out.println("--------------------------------------------------");
+        System.out.println("----------------SUPERMERCADO TERNURA------------- ");
+        System.out.println("Escolha um opção para começar:                    ");
+        System.out.println(" 1-Armazenamento de produtos");
+        System.out.println(" 2-Sistema de Controle de vendas");
+        System.out.println(" 3-Balancete de vendas.");
+        System.out.println(" 4-Cadastro de Vendedores");
+        System.out.println(" 5-Cadastro de Clientes");
+        System.out.println(" 6- SAIR ");
     }
     
 }
