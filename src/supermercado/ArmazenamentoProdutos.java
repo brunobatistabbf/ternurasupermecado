f4ea3382a8adbd24452a1ea22d876d5cd1c93ce6
brunofagner <60938640+brunofagner@users.noreply.github.com>
2021-01-17 15:26:08 -0300

    Atualizações

--- src/supermercado/ArmazenamentoProdutos.java
+++ src/supermercado/ArmazenamentoProdutos.java
@@ -2,19 +2,19 @@ package supermercado;
 
 public class ArmazenamentoProdutos {
 //Atributos
-public String marca;
-protected int quantidade;
+private String nome;
+private int quantidade;
 private int codigo;
-public float preco;
+private double preco;
 public boolean valido;
 
 //metodos personalizados do produto
  public void mostrarproduto(){
-     System.out.println("-------------- Produto Cadastrado---------------::::");
+     System.out.println("-------------- Produto Cadastrado----------------::::");
      System.out.println("Quantidade :       " +getQuantidade());
      System.out.println("Preço da unidade : " +getPreco());
-     System.out.println("Marca :            " +getMarca());
-     System.out.println("Dentro da validade:" +getValido());
+     System.out.println("Nome :            " +getNome());
+     System.out.println("Codigo do produto:" +getCodigo());
      System.out.println("-------------------------------------------------::::");
  }
  
@@ -23,7 +23,7 @@ public boolean valido;
      this.setQuantidade(q);
      this.setCodigo(c);
      this.setPreco(p);
-     this.setMarca(m);
+     this.setNome(m);
      this.mostrarproduto();
      
     }
@@ -40,17 +40,20 @@ public boolean valido;
     
  //construtor
 
-    public ArmazenamentoProdutos() {
-        this.valido = false;
-        
-    }
-//metodos personalizados
-    public String getMarca() {
-        return marca;
+    public ArmazenamentoProdutos(String nome, int quantidade, int codigo, double preco) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.codigo = codigo;
+        this.preco = preco;
     }
 
-    public void setMarca(String marca) {
-        this.marca = marca;
+//metodos personalizados
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public int getQuantidade() {
@@ -69,11 +72,11 @@ public boolean valido;
         this.codigo = codigo;
     }
 
-    public float getPreco() {
+    public double getPreco() {
         return preco;
     }
 
-    public void setPreco(float preco) {
+    public void setPreco(double preco) {
         this.preco = preco;
     }
 
--- src/supermercado/Supermercado.java
+++ src/supermercado/Supermercado.java
@@ -1,4 +1,6 @@
 package supermercado;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 //%%Lista de Funcionalidades:%%
 //-Armazenamento de produtos
@@ -10,11 +12,12 @@ import java.util.Scanner;
 public class Supermercado {
     
     public static void main(String[] args) {
-        int  opcao;
+        int opcao;
+        int id = 0;
         Scanner sc = new Scanner(System.in);
-        
+        List<ArmazenamentoProdutos> produtos = new ArrayList<ArmazenamentoProdutos>();
         while (true) {
-           
+            while(true){
                 System.out.println("--------------------------------------------------");
                 System.out.println("----------------SUPERMERCADO TERNURA------------- ");
                 System.out.println("Escolha um opção para começar:                    ");
@@ -25,10 +28,10 @@ public class Supermercado {
                 System.out.println(" 5-Cadastro de Clientes");
                 System.out.println(" 6- SAIR ");
                 opcao = sc.nextInt();
-            while(true){
+
             switch (opcao) {
                 case 1:
-                    produto();
+                    produto(id, produtos);
                 case 2:
                     break;
                 case 3:
@@ -45,44 +48,67 @@ public class Supermercado {
                 }
             }
         }
-        
-        
+
+
     }
-    public static void produto() {
-       
-        System.out.println("--------------------------------------------");
-        System.out.println("1- Armazene um produto");
-        System.out.println("2- Remover produto");
-        
-        Scanner sc1 = new Scanner(System.in);
-        int opcaop;
-        opcaop = sc1.nextInt();
-        ArmazenamentoProdutos p1 = new ArmazenamentoProdutos();
-        if (opcaop ==1){
-            System.out.println("Insira as seguintes informações");
-            System.out.println("Quantidade: ");
-            int q = sc1.nextInt();
-            System.out.println("Codigo do produto:");
-            int c = sc1.nextInt();
-            System.out.println("Preco: ");
-            float p = sc1.nextFloat();
-            System.out.println("Marca: ");
-            String m = sc1.nextLine();
-            p1.armazenarproduto(q, c, p, m);
-            p1.mostrarproduto();
-            System.out.println("Produto Criado com sucesso!!!");
+    public static void produto(int id, List<ArmazenamentoProdutos> produtos) {
+        while (true) {
+            System.out.println("--------------------------------------------");
+            System.out.println("1- Armazene um produto");
+            System.out.println("2- Remover produto");
+            System.out.println("3- Mostrar produtos cadastrados");
+            System.out.println("4- Sair");
+
+            Scanner sc1 = new Scanner(System.in);
+            int opcaop;
+            opcaop = sc1.nextInt();
+
+            if (opcaop == 1) {
+
+                    System.out.println("Insira as seguintes informações");
+                    System.out.println("Quantidade: ");
+                    int quant = sc1.nextInt();
+
+                    System.out.println("Codigo do produto:");
+                    int cod = sc1.nextInt();
+
+                    System.out.println("Preco: ");
+                    float preco = sc1.nextFloat();
+
+                    System.out.println("Nome do produto: ");
+                    String nome = sc1.next();
+
+                    produtos.add(new ArmazenamentoProdutos(nome, quant, cod, preco));
+                    produtos.get(id).setQuantidade(quant);
+                    produtos.get(id).setQuantidade(cod);
+                    produtos.get(id).setPreco(preco);
+                    produtos.get(id).setNome(nome);
+
+                    System.out.println("Produto Criado com sucesso!!!");
+                    id++;
+
+            } else if (opcaop == 2) {
+                System.out.println("Qual o id do produto que deseja remover?");
+                int idp = sc1.nextInt();
+                produtos.remove(idp);
+            }else if (opcaop == 3){
+                for (ArmazenamentoProdutos p : produtos){
+                    System.out.println("Nome:"+p.getNome());
+                    System.out.println("Codigo: "+p.getCodigo());
+                    System.out.println("Preço: "+ p.getPreco());
+                    System.out.println("Quantidade armazenada: "+p.getQuantidade());
+                    System.out.println("----------------------------::");
+                }
+            } else if (opcaop == 4)
+                break;
         }
-        else if (opcaop == 2){
-            p1.removerproduto();
-        }
-        p1.mostrarproduto();
     }
     public static void vendedor() {
-               
+
         System.out.println("--------------------------------------------");
         System.out.println("1- Cadastrar vendedor");
         System.out.println("2- Demitir vendedor");
-        
+
         Scanner sc = new Scanner(System.in);
         int opcaop;
         opcaop = sc.nextInt();
@@ -107,7 +133,7 @@ public class Supermercado {
         v1.mostrarvendedor();
     }
     public static void cliente() {
-               
+
         System.out.println("--------------------------------------------");
         System.out.println("1- Cadastrar cliente");
         System.out.println("2- Remover Cliente");
